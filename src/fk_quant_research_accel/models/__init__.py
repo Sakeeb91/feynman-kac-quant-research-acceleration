0ca2075fae8a9a4c4d90ef49866ac0f278eb9ed4
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-19 18:46:10 -0500

    feat(models): export public model types and helpers

--- src/fk_quant_research_accel/models/__init__.py
+++ src/fk_quant_research_accel/models/__init__.py
@@ -1,3 +1,29 @@
 """Domain models for durable batch execution and persistence."""
 
-__all__: list[str] = []
+from .enums import LogLevel, ScenarioStatus
+from .ids import BatchRunId, ScenarioRunId, generate_batch_run_id, generate_scenario_run_id
+from .manifest import (
+    ManifestMetadata,
+    ReproducibilityInfo,
+    RunManifest,
+    capture_environment,
+    capture_git_info,
+    write_manifest,
+)
+from .result import ScenarioResult
+
+__all__ = [
+    "BatchRunId",
+    "ScenarioRunId",
+    "generate_batch_run_id",
+    "generate_scenario_run_id",
+    "ScenarioStatus",
+    "LogLevel",
+    "ManifestMetadata",
+    "ReproducibilityInfo",
+    "RunManifest",
+    "write_manifest",
+    "capture_git_info",
+    "capture_environment",
+    "ScenarioResult",
+]
