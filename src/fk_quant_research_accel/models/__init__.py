d2da6f10588d74bc4dc1f685561139a44e352143
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-20 11:52:00 -0500

    feat(02-01): export experiment manifest and hashing models

--- src/fk_quant_research_accel/models/__init__.py
+++ src/fk_quant_research_accel/models/__init__.py
@@ -1,6 +1,16 @@
 """Domain models for durable batch execution and persistence."""
 
-from .enums import LogLevel, ScenarioStatus
+from .enums import LogLevel, OptionType, ScenarioStatus, ScoringStrategy
+from .experiment import (
+    BatchRunConfig,
+    ExperimentManifest,
+    ModelSweepConfig,
+    OutputConfig,
+    ScoringConfig,
+    ScenarioGridConfig,
+    load_manifest,
+)
+from .hashing import content_hash
 from .ids import BatchRunId, ScenarioRunId, generate_batch_run_id, generate_scenario_run_id
 from .manifest import (
     ManifestMetadata,
@@ -19,6 +29,16 @@ __all__ = [
     "generate_scenario_run_id",
     "ScenarioStatus",
     "LogLevel",
+    "OptionType",
+    "ScoringStrategy",
+    "ScenarioGridConfig",
+    "ModelSweepConfig",
+    "BatchRunConfig",
+    "ScoringConfig",
+    "OutputConfig",
+    "ExperimentManifest",
+    "load_manifest",
+    "content_hash",
     "ManifestMetadata",
     "ReproducibilityInfo",
     "RunManifest",
