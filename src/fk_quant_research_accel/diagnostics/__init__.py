c18a400c48c59238c55a1ad26aa881e85afd373c
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-22 16:06:45 -0500

    feat(diagnostics): add diagnostics package exports

---
+++ src/fk_quant_research_accel/diagnostics/__init__.py
@@ -0,0 +1,6 @@
+"""Convergence diagnostics helpers."""
+
+from fk_quant_research_accel.diagnostics.health import diagnose_convergence
+from fk_quant_research_accel.models.enums import ConvergenceHealth
+
+__all__ = ["diagnose_convergence", "ConvergenceHealth"]
