680aaba73b4a5e99821a0efa64aa5b6c0f212344
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-25 11:03:10 -0500

    phase6-01: export protocol and registry APIs

--- src/fk_quant_research_accel/problems/__init__.py
+++ src/fk_quant_research_accel/problems/__init__.py
@@ -1,1 +1,13 @@
 """Problem-type extensibility module."""
+
+from .protocol import BaseProblemSpec, ProblemParams, ProblemSpec
+from .registry import get_problem_spec, list_problem_ids, register_problem
+
+__all__ = [
+    "ProblemSpec",
+    "BaseProblemSpec",
+    "ProblemParams",
+    "get_problem_spec",
+    "register_problem",
+    "list_problem_ids",
+]
