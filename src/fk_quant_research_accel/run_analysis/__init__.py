56627531defad310bfee3dc45ff8d0fd7f808bcf
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-24 20:51:03 -0500

    feat(run-analysis): export comparison helpers from package init

--- src/fk_quant_research_accel/run_analysis/__init__.py
+++ src/fk_quant_research_accel/run_analysis/__init__.py
@@ -3,9 +3,14 @@
 from .resolver import resolve_run_id
 from .queries import list_runs_with_metrics
 from .formatters import get_effective_format
+from .comparison import align_scenarios, compute_comparison, delta_abs, delta_pct
 
 __all__ = [
     "resolve_run_id",
     "list_runs_with_metrics",
     "get_effective_format",
+    "align_scenarios",
+    "compute_comparison",
+    "delta_abs",
+    "delta_pct",
 ]
